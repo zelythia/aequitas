7e811b83062c349750b7f9571b617abcdd6a3cb4
zelythia <zelythia@gmail.com>
2023-09-25 17:08:49 +0200

    Added essence items
    Took 20 seconds

--- src/main/java/net/zelythia/aequitas/Aequitas.java
+++ src/main/java/net/zelythia/aequitas/Aequitas.java
@@ -50,11 +50,26 @@ public class Aequitas implements ModInitializer {
     public static final Block CONDUIT_BLOCK;
     public static final BlockItem CONDUIT_BLOCK_ITEM;
 
-    public static final Block CATALYST_BLOCK;
-    public static final BlockItem CATALYST_BLOCK_ITEM;
+    public static final Block CATALYST_BLOCK_I;
+    public static final Block CATALYST_BLOCK_II;
+    public static final Block CATALYST_BLOCK_III;
+    public static final BlockItem CATALYST_BLOCK_ITEM_I;
+    public static final BlockItem CATALYST_BLOCK_ITEM_II;
+    public static final BlockItem CATALYST_BLOCK_ITEM_III;
 
     public static final Item SAMPLING_PEDESTAL_CORE;
     public static final Item CRAFTING_PEDESTAL_CORE;
+    public static final Item PRIMAL_ESSENCE;
+    public static final Item PRIMORDIAL_ESSENCE;
+    public static final Item PRISTINE_ESSENCE;
+
+    public static final Block PRIMAL_ESSENCE_BLOCK;
+    public static final Block PRIMORDIAL_ESSENCE_BLOCK;
+    public static final Block PRISTINE_ESSENCE_BLOCK;
+    public static final BlockItem PRIMAL_ESSENCE_BLOCK_ITEM;
+    public static final BlockItem PRIMORDIAL_ESSENCE_BLOCK_ITEM;
+    public static final BlockItem PRISTINE_ESSENCE_BLOCK_ITEM;
+
 
     public static final Identifier PEDESTAL = new Identifier(MOD_ID, "pedestal");
     public static final Block PEDESTAL_BLOCK;
@@ -94,7 +109,11 @@ public class Aequitas implements ModInitializer {
 
         //Blocks
         CONDUIT_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "conduit_block"), new ConduitBlock(AbstractBlock.Settings.of(Material.STONE).luminance((blockState) -> (Boolean)blockState.get(ACTIVE_BLOCK_PROPERTY) ? 13 : 0)));
-        CATALYST_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "catalyst"), new CatalystBlock(AbstractBlock.Settings.of(Material.METAL).luminance((blockState) -> (Boolean)blockState.get(ACTIVE_BLOCK_PROPERTY) ? 15 : 0)));
+        CATALYST_BLOCK_I = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "catalyst_1"), new CatalystBlock(AbstractBlock.Settings.of(Material.METAL).luminance((blockState) -> (Boolean)blockState.get(ACTIVE_BLOCK_PROPERTY) ? 15 : 0)));
+        CATALYST_BLOCK_II = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "catalyst_2"), new CatalystBlock(AbstractBlock.Settings.of(Material.METAL).luminance((blockState) -> (Boolean)blockState.get(ACTIVE_BLOCK_PROPERTY) ? 15 : 0)));
+        CATALYST_BLOCK_III = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "catalyst_3"), new CatalystBlock(AbstractBlock.Settings.of(Material.METAL).luminance((blockState) -> (Boolean)blockState.get(ACTIVE_BLOCK_PROPERTY) ? 15 : 0)));
+
+
         PEDESTAL_BLOCK = Registry.register(Registry.BLOCK, PEDESTAL, new PedestalBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.BLACK).requiresTool().strength(1.25F, 4.2F)));
         CRAFTING_PEDESTAL_BLOCK = Registry.register(Registry.BLOCK, CRAFTING_PEDESTAL, new CraftingPedestalBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.BLACK).requiresTool().strength(1.25F, 4.2F)));
         SAMPLING_PEDESTAL_BLOCK = Registry.register(Registry.BLOCK, SAMPLING_PEDESTAL, new SamplingPedestalBlock(AbstractBlock.Settings.of(Material.STONE, MaterialColor.BLACK).requiresTool().strength(1.25F, 4.2F)));
@@ -103,9 +122,17 @@ public class Aequitas implements ModInitializer {
         COLLECTION_BOWL_BLOCK_II = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "collection_bowl_2"), new CollectionBowlBlock(AbstractBlock.Settings.of(Material.GLASS),9));
         COLLECTION_BOWL_BLOCK_III = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "collection_bowl_3"), new CollectionBowlBlock(AbstractBlock.Settings.of(Material.GLASS),15));
 
+        PRIMAL_ESSENCE_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "primal_essence_block"), new Block(AbstractBlock.Settings.of(Material.GLASS, MaterialColor.WHITE)));
+        PRIMORDIAL_ESSENCE_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "primordial_essence_block"), new Block(AbstractBlock.Settings.of(Material.GLASS, MaterialColor.WHITE)));
+        PRISTINE_ESSENCE_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "pristine_essence_block"), new Block(AbstractBlock.Settings.of(Material.GLASS, MaterialColor.WHITE)));
+
         //Items
         CONDUIT_BLOCK_ITEM = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "conduit_block"), new BlockItem(CONDUIT_BLOCK, new Item.Settings().group(ITEM_GROUP)));
-        CATALYST_BLOCK_ITEM = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "catalyst"), new BlockItem(CATALYST_BLOCK, new Item.Settings().group(ITEM_GROUP)));
+        CATALYST_BLOCK_ITEM_I = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "catalyst_1"), new BlockItem(CATALYST_BLOCK_I, new Item.Settings().group(ITEM_GROUP)));
+        CATALYST_BLOCK_ITEM_II = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "catalyst_2"), new BlockItem(CATALYST_BLOCK_II, new Item.Settings().group(ITEM_GROUP)));
+        CATALYST_BLOCK_ITEM_III = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "catalyst_3"), new BlockItem(CATALYST_BLOCK_III, new Item.Settings().group(ITEM_GROUP)));
+
+
         SAMPLING_PEDESTAL_CORE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "sampling_pedestal_core"), new Item(new Item.Settings().group(ITEM_GROUP)));
         CRAFTING_PEDESTAL_CORE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "crafting_pedestal_core"), new Item(new Item.Settings().group(ITEM_GROUP)));
 
@@ -117,7 +144,12 @@ public class Aequitas implements ModInitializer {
         COLLECTION_BOWL_BLOCK_ITEM_II = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "collection_bowl_2"), new BlockItem(COLLECTION_BOWL_BLOCK_II, new Item.Settings().group(ITEM_GROUP)));
         COLLECTION_BOWL_BLOCK_ITEM_III = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "collection_bowl_3"), new BlockItem(COLLECTION_BOWL_BLOCK_III, new Item.Settings().group(ITEM_GROUP)));
 
-
+        PRIMAL_ESSENCE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "primal_essence"), new Item(new Item.Settings().group(ITEM_GROUP)));
+        PRIMORDIAL_ESSENCE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "primordial_essence"), new Item(new Item.Settings().group(ITEM_GROUP)));
+        PRISTINE_ESSENCE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "pristine_essence"), new Item(new Item.Settings().group(ITEM_GROUP)));
+        PRIMAL_ESSENCE_BLOCK_ITEM = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "primal_essence_block"), new BlockItem(PRIMAL_ESSENCE_BLOCK, new Item.Settings().group(ITEM_GROUP)));
+        PRIMORDIAL_ESSENCE_BLOCK_ITEM = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "primordial_essence_block"), new BlockItem(PRIMORDIAL_ESSENCE_BLOCK, new Item.Settings().group(ITEM_GROUP)));
+        PRISTINE_ESSENCE_BLOCK_ITEM = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "pristine_essence_block"), new BlockItem(PRISTINE_ESSENCE_BLOCK, new Item.Settings().group(ITEM_GROUP)));
 
         //Entities
         CRAFTING_PEDESTAL_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, CRAFTING_PEDESTAL, BlockEntityType.Builder.create(CraftingPedestalBlockEntity::new, CRAFTING_PEDESTAL_BLOCK).build(null));
